de8ee996458952302df5f6847884542610d9ba9c
NAnastasiyaaa <maherus16@gmail.com>
2022-09-11 17:05:41 +0300

    add server for random images

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+
+app.use(cors());
+app.use(express.static("cats"));
+app.use("/cats", express.static("cats"));
+
+app.get("/api/randomimage", (req, res) => {
+  fs.readdir("cats/", (err, files) => {
+    const random = Math.floor(Math.random() * files.length);
+
+    res.json({ status: true, image: files[random] });
+  });
+});
+
+app.listen(5000, () => {
+  console.log("server started");
+});
